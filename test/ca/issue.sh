9f498dc0bb197b812a16d3cc5a25452a6df21f0f
Alon Bar-Lev <alon.barlev@gmail.com>
2023-05-24 22:17:25 +0300

    tests: support easyrsa-3.1

--- test/ca/issue.sh
+++ test/ca/issue.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 abs_builddir="${abs_builddir:-$(dirname "$0")}"
-. "${abs_builddir}/vars"
+. "${abs_builddir}/ca-vars"
 
 die() {
 	local m="$1"
@@ -14,8 +14,8 @@ NAME="$1"; shift
 CERT="$1"; shift
 KEY="$1"; shift
 
-"${EASYRSA}" --vars="${abs_builddir}/subca.vars" gen-req "${NAME}" nopass || die "req"
-"${EASYRSA}" --vars="${abs_builddir}/subca.vars"  --subject-alt-name="URI:test:${NAME}" sign-req "${TEMPLATE}" "${NAME}" nopass || die "sign"
+"${EASYRSA}" --pki-dir="${abs_builddir}/subca.pki" gen-req "${NAME}" nopass || die "req"
+"${EASYRSA}" --pki-dir="${abs_builddir}/subca.pki" --subject-alt-name="URI:test:${NAME}" sign-req "${TEMPLATE}" "${NAME}" nopass || die "sign"
 
 "${OPENSSL}" x509 -in "${abs_builddir}/subca.pki/issued/${NAME}.crt" -inform PEM -out "${CERT}" -outform DER || die "export.cert"
 "${OPENSSL}" pkcs8 -in "${abs_builddir}/subca.pki/private/${NAME}.key" -inform PEM -out "${KEY}" -outform DER -nocrypt || die "export.key"
